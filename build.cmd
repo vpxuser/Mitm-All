4d1289c1c2c3ae21c48471fd69e0a84ca7ca8a98
unknown <20809609@dw.gdbbk.com>
2024-09-14 10:26:19 +0800

    update windows build script

--- build.cmd
+++ build.cmd
@@ -1,1 +1,34 @@
+@echo off
+chcp 65001 > nul
+setlocal
+
+REM 构建 Go 程序
 go build -o socks2https.exe main.go
+
+REM 检查 go build 是否成功
+if %errorlevel% neq 0 (
+    echo "构建失败"
+    exit /b %errorlevel%
+)
+
+REM 打包文件和目录
+tar -cvf socks2https.tar config socks2https.exe
+
+REM 检查 tar 命令是否成功
+if %errorlevel% neq 0 (
+    echo "打包失败"
+    exit /b %errorlevel%
+)
+
+REM 删除可执行文件
+del socks2https.exe
+
+REM 检查删除操作是否成功
+if %errorlevel% neq 0 (
+    echo "删除失败"
+    exit /b %errorlevel%
+)
+
+echo "操作完成"
+
+endlocal
